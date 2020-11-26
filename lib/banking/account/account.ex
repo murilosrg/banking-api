0d384324cdc203507949dd23533fb9e9c0267061
murilosrg <muriilo@live.it>
2020-11-26 18:57:30 -0300

    feat: add unit tests

--- lib/banking/account/account.ex
+++ lib/banking/account/account.ex
@@ -6,9 +6,9 @@ defmodule Banking.Account do
   use Ecto.Schema
 
   @primary_key {:id, Ecto.UUID, autogenerate: true}
-  @foreign_key_type :binary_id
+  @foreign_key_type Ecto.UUID
   schema "accounts" do
-    field :balance, :decimal, default: 1000.0
+    field :balance, :decimal, default: 1000
 
     belongs_to :user, Banking.User
 
