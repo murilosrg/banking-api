c0655f9b59f45e5bf18c30fddbdb72645636e2f2
murilosrg <muriilo@live.it>
2020-12-03 11:17:11 -0300

    fix: change admin pass

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -3,7 +3,7 @@ Banking.Repo.insert!(
     email: "admin@email.com",
     employee: true,
     name: "Admin",
-    password_hash: "$2b$12$Tx/1Jc2AXiLCNJX8dFUoneVzVQzqgb1Y3OeIYCKO6B66Yde3hDwym"
+    password_hash: "$$2b$12$h5pn4F3T67J.2nHb8XitfufnAcrRWF6wdl0.8ub3hvGyF.1vAmlCK"
   },
   on_conflict: :replace_all,
   conflict_target: :email
